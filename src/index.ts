71c45ac4a228b29ea8ad99b36a6f3600c540d87a
Rishi Kothari <itsrishikothari@gmail.com>
2021-10-02 23:38:10 -0400

    add new server

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,35 @@
-import { signing_secret, token, name } from './config';
-import { App } from "@slack/bolt";
-import { filterDM, filterNoBotMessages, filterChannel } from './middleware/index';
+import 'reflect-metadata'
+
+import { signing_secret, token, name } from './config'
+import { App } from '@slack/bolt'
+import {
+	filterDM,
+	filterNoBotMessages,
+	filterChannel,
+} from './middleware/index'
 import * as features from './features/index'
 
-const app = new App({
-    signingSecret: signing_secret,
-    token: token
-});
+import { createConnection } from 'typeorm'
+import { User } from './entities/user'
+import { Report } from './entities/report'
 
-(async () => {
-      // Start your app
-  await app.start(process.env.PORT || 3000);
+export const app = new App({
+	signingSecret: signing_secret,
+	token: token,
+})
+;(async () => {
+	// Start your app
+	await app.start(process.env.PORT || 3000)
+	await createConnection({
+		url: process.env.DATABASE_URL,
+		type: 'postgres',
+		entities: [User, Report],
+		synchronize: true,
+	})
+	console.log(`${name} is running! 🔥`)
 
-  console.log(`${name} is running! 🔥`);
-
-  for (const [ feature, handler ] of Object.entries(features)) {
-      handler(app);
-      console.log(`Feature "${feature}" has been loaded.`)
-  }
-  
+	for (const [feature, handler] of Object.entries(features)) {
+		handler(app)
+		console.log(`Feature "${feature}" has been loaded.`)
+	}
 })()
