2fc6bf4be8bedae7772180fc9e422e05d02a7a57
Rishi Kothari <itsrishikothari@gmail.com>
2021-10-02 23:38:01 -0400

    remove feature template

--- src/features/feature1.ts
+++
@@ -1,9 +0,0 @@
-import { App } from '@slack/bolt';
-
-const feature1 = async (app: App) => {
-    app.message("listener1", async ({message, say}) => {
-        say(`Hello World! <@${message.user}> said: ${message.text}`);
-    })
-}
-
-export default feature1;
